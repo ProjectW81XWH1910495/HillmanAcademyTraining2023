35c3f1d9e6614aa3d0fc494c75f6480edecc03c2
omarorajain <92062635+omarorajain@users.noreply.github.com>
2023-06-16 12:21:15 -0400

    Plot Trend Driver

--- Working_dir/Plot_Trend_Driver.py
+++ Working_dir/Plot_Trend_Driver.py
@@ -2,7 +2,6 @@ import sys
 import os
 import pandas as pd
 
-sys.path.append(os.path.abspath(r"H:\My Drive\Education\Internships\Hillman\2023\keras\utils"))
 from plottrend import *
 
 filePath = r"H:\My Drive\Education\Internships\Hillman\2023\HillmanAcademyTraining2023\experiment_results\15year_stage1version2_momen_version2_merge_SGD.csv"
@@ -11,8 +10,9 @@ resultPath = r"H:\My Drive\Education\Internships\Hillman\2023\HillmanAcademyTrai
 dictionary_column = 'parameters_and_values'
 
 x_label = 'momen'
-y_label = 'test_auc'
-title = 'Momemtum vs AUC'
+y_label = 'mean_test_auc'
+title = 'test'
+time = True
 
 expand_dictionary_column(filePath,dictionary_column,resultPath)
 
@@ -21,4 +21,9 @@ new_column = df[y_label]
 new_column_name=y_label
 add_new_column_tocsv(resultPath,new_column,new_column_name)
 
-plot_one_background_setting(resultPath, x_label, y_label, title)
+fig = plot_many_background_settings(resultPath, x_label, y_label, title, time)
+
+dpi = 400
+scale_factor = dpi / 96
+pio.write_image(fig, f'x_{x_label}_y_{y_label}_trend_plot.png', scale=scale_factor)
+fig.show()
